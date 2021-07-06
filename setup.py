3d7a3530a7e65d4035daf89a836c1cb51dec5adc
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-06 17:27:42 +0200

    Update the documentation and the setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,36 @@
-from distutils.core import setup
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
       name             = 'bumphub',
       version          = '1.2.0',
-      description      = 'Tools for building doc and incrementing python package version numbers',
-      long_description = open('README.md').read(),
-      long_description_content_type = 'text/markdown',
+      description      = 'Scripts to automate distribution and packaging of'
+                         ' python modules on PyPI and conda ',
       license          = 'MIT',
-      url              = 'https://github.com/xapple/bumphub/',
+      url              = 'http://github.com/xapple/bumphub/',
       author           = 'Lucas Sinclair',
       author_email     = 'lucas.sinclair@me.com',
-      install_requires = ['plumbing>=2.8.0', 'autopaths>=1.4.1', 'sh']
-      scripts          = ['bumphub/pybump'],
-    )
+      packages         = find_namespace_packages(),
+      install_requires = ['plumbing>=2.10.5', 'autopaths>=1.5.0', 'sh'],
+      python_requires  = ">=3.8",
+      scripts          = ['pybump', 'anacondabump'],
+      long_description = readme,
+      long_description_content_type = 'text/markdown',
+      include_package_data = True,
+)
