c5e38200685fb428f1ce6ac1f7eede2f7152e394
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2026-01-01 13:00:50 +0800

    Rename some args and separate each strategy

--- conda_scripts/__init__.py
+++ conda_scripts/__init__.py
@@ -10,36 +10,36 @@ import os, sys, argparse
 # First party modules #
 from autopaths import Path
 
-
-def parse_common_args(description):
+###############################################################################
+def get_args(description):
     # Create a shell parser #
     parser = argparse.ArgumentParser(description=description)
 
     # Ask for the main argument #
-    help_msg = "The name of the module to process."
+    help_msg = "The name of the project to process."
     parser.add_argument("module", nargs='?', default=None, help=help_msg, type=str)
 
     # Optional testing mode
     parser.add_argument("--test", action='store_true')
 
     # Parse the shell arguments #
-    args         = parser.parse_args()
+    args = parser.parse_args()
     path_or_name = args.module
-    test_mode    = args.test
+    test_mode = args.test
 
     # We can accept module names, filesystem paths, or default to CWD #
     if path_or_name is None:
-        base_dir    = Path(os.getcwd())
-        module_name = base_dir.name
+        base_dir = Path(os.getcwd())
+        proj_name = base_dir.name
     elif '/' in path_or_name:
-        base_dir    = Path(path_or_name)
-        module_name = base_dir.name
+        base_dir = Path(path_or_name)
+        proj_name = base_dir.name
     else:
-        base_dir    = Path("~/repos/%s/" % path_or_name)
-        module_name = path_or_name
+        base_dir = Path("~/repos/%s/" % path_or_name)
+        proj_name = path_or_name
 
     # The python code directory #
-    code_dir = base_dir + module_name + '/'
+    code_dir = base_dir + proj_name + '/'
 
     # Packaging files #
     toml_path  = base_dir + 'pyproject.toml'
@@ -52,4 +52,4 @@ def parse_common_args(description):
     # Change directory #
     os.chdir(base_dir)
 
-    return args, base_dir, module_name, code_dir, toml_path, test_mode
+    return base_dir, proj_name, code_dir, toml_path, test_mode
